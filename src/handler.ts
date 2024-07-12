97444c9dae49cc71c89fae9e52f734759b450364
root <root@ubuntu2204.ubuntu2204>
2024-07-12 10:18:11 +0800

    Fix bug
    usage:
    
    docker pull nvcr.io/nvidia/tritonserver:21.09-py3                                    ->       docker pull proxy.foo.bar/nvcr.io/nvidia/tritonserver:21.09-py3
    docker pull nginx                                                                    ->       docker pull proxy.foo.bar/nginx
    docker pull library/nginx                                                            ->       docker pull proxy.foo.bar/library/nginx
    docker pull docker.io/library/nginx                                                  ->       docker pull proxy.foo.bar/docker.io/library/nginx
    docker pull gcr.io/ml-pipeline/workflow-controller:v3.3.8-license-compliance         ->       docker pull proxy.foo.bar/gcr.io/ml-pipeline/workflow-controller:v3.3.8-license-compliance
    docker pull kserve/storage-initializer:v0.10.0                                       ->       docker pull proxy.foo.bar/kserve/storage-initializer:v0.10.0
    docker pull kserve/sklearnserver:v0.10.0                                             ->       docker pull proxy.foo.bar/kserve/sklearnserver:v0.10.0

--- src/handler.ts
+++ src/handler.ts
@@ -5,11 +5,14 @@ const PROXY_HEADER_ALLOW_LIST: string[] = ["accept", "user-agent", "accept-encod
 
 const validActionNames = new Set(["manifests", "blobs", "tags", "referrers"])
 
-// const ORG_NAME_BACKEND:{ [key: string]: string; } = {
-//   "gcr": "https://gcr.io",
-//   "k8sgcr": "https://k8s.gcr.io",
-//   "quay": "https://quay.io",
-// }
+const ORG_NAME_BACKEND:{ [key: string]: string; } = {
+  "gcr.io": "https://gcr.io",
+  "k8s.gcr.io": "https://k8s.gcr.io",
+  "quay.io": "https://quay.io",
+  "docker.io": "https://registry-1.docker.io",
+  "ghcr.io": "https://ghcr.io",
+  "nvcr.io": "https://nvcr.io"
+}
 
 const DEFAULT_BACKEND_HOST: string = "https://registry-1.docker.io"
 
@@ -28,6 +31,12 @@ function copyProxyHeaders(inputHeaders: Headers) : Headers {
 }
 
 function orgNameFromPath(pathname: string): string|null {
+  let splitedPath = pathname.split("/");
+
+  // /v2/gcr.io/yyyy/repo/manifests/xxx
+  if (splitedPath.length === 7 && splitedPath[0] === "" && splitedPath[1] === "v2") {
+    return splitedPath[2].toLowerCase()
+  }
   // const splitedPath: string[] = pathname.split("/", 3)
   // if (splitedPath.length === 3 && splitedPath[0] === "" && splitedPath[1] === "v2") {
   //   return splitedPath[2].toLowerCase()
@@ -36,37 +45,62 @@ function orgNameFromPath(pathname: string): string|null {
 }
 
 function hostByOrgName(orgName: string|null): string {
-  // if (orgName !== null && orgName in ORG_NAME_BACKEND) {
-  //   return ORG_NAME_BACKEND[orgName]
-  // }
+  if (orgName !== null && orgName in ORG_NAME_BACKEND) {
+    return ORG_NAME_BACKEND[orgName]
+  }
+
   return DEFAULT_BACKEND_HOST
 }
 
 function rewritePath(orgName: string | null, pathname: string): string {
   let splitedPath = pathname.split("/");
+  /*
+    case 1: (without docker.io without library)
+    /v2/repo/manifests/xxx -> /v2/library/repo/manifests/xxx
+    /v2/repo/blobs/xxx -> /v2/library/repo/blobs/xxx
 
-  // /v2/repo/manifests/xxx -> /v2/library/repo/manifests/xxx
-  // /v2/repo/blobs/xxx -> /v2/library/repo/blobs/xxx
+    case 2: (with docker.io without library)
+    /v2/docker.io/repo/manifests/xxx -> /v2/library/repo/manifests/xxx
+    /v2/docker.io/repo/blobs/xxx -> /v2/library/repo/blobs/xxx
+
+    case 3: (without docker.io with library)
+    /v2/library/repo/manifests/xxx -> /v2/library/repo/manifests/xxx
+
+    case 4: (with docker.io with library)
+    /v2/docker.io/library/repo/blobs/xxx -> /v2/library/repo/blobs/xxx
+
+    case 5: other
+    /v2/gcr.io/library/repo/blobs/xxx -> /v2/library/repo/blobs/xxx
+  */
   if (orgName === null && splitedPath.length === 5 && validActionNames.has(splitedPath[3])) {
+    // case 1
     splitedPath = [splitedPath[0], splitedPath[1], "library", splitedPath[2], splitedPath[3], splitedPath[4]]
+  } else if (orgName === null && splitedPath.length === 6 && validActionNames.has(splitedPath[4])) {
+    // case3
+    ;
+  } else if (orgName === "docker.io" && splitedPath.length === 6 && validActionNames.has(splitedPath[4])) {
+    // case 2
+    splitedPath = [splitedPath[0], splitedPath[1], "library", splitedPath[3], splitedPath[4], splitedPath[5]]
+  } else if (orgName !== null && splitedPath.length === 7) {
+    // case 4 & case 5
+    splitedPath = [splitedPath[0], splitedPath[1], splitedPath[3], splitedPath[4], splitedPath[5], splitedPath[6]]
   }
 
-  return splitedPath.join("/")
 
-  // if (orgName === null || !(orgName in ORG_NAME_BACKEND)) {
-  //   return pathname
-  // }
-  //
-  // const cleanSplitedPath = splitedPath.filter(function(value: string, index: number) {
-  //   return value !== orgName || index !== 2;
-  // })
-  // return cleanSplitedPath.join("/")
+  return splitedPath.join("/")
 }
 
 async function handleRegistryRequest(request: Request): Promise<Response> {
   const reqURL = new URL(request.url)
   const orgName = orgNameFromPath(reqURL.pathname)
   const pathname = rewritePath(orgName, reqURL.pathname)
+  /*
+    proxy.foo.bar/gcr.io/ml-pipeline/frontend:2.0.0-alpha.7
+    proxy.foo.bar/quay.io/jetstack/cert-manager-webhook:v1.10.1
+    proxy.foo.bar/docker.io/kubeflownotebookswg/tensorboards-web-app:v1.7.0
+    proxy.foo.bar/kubeflownotebookswg/jupyter-tensorflow-cuda-full:v1.7.0
+    proxy.foo.bar/kubeflownotebookswg/tensorboards-web-app:v1.7.0
+  */
   const host = hostByOrgName(orgName)
   const tokenProvider = new TokenProvider()
   const backend = new Backend(host, tokenProvider)
